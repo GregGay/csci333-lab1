e38e0174a807f978d66e26f9d44ded61a32ce34c
GregGay <ggay@unca.edu>
2012-09-05 00:26:39 -0400

    Problem 1 from project euler is solved.

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,22 @@
 
 using std::cout;
 using std::endl;
+int euler(int n);
 
 int main() {
-	cout<<"Hello World!"<<endl;
+	//cout<<"Hello World!"<<endl;
+
+	//Problem 1
+	cout<<"The answer is: "<<euler(1000)<<endl;
+	return 0;
+}
+
+int euler(int n) {
+	int sum = 0;
+	for (int i =0; i<n; i++) {
+		if (i%3==0 || i%5==0) {
+			sum+=i;
+		}
+	}
+	return sum;
 }
